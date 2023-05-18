eca7da9446b0354dd44d46dc2312a9bd2aae0b5d
Nick <54436939+Nickpatel008@users.noreply.github.com>
2023-05-18 12:58:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DNS_webclone
 1.1.1.1 DNS website clone cloudflare
+
+## This website craete for just learning purpose.
--- js/app.js
+++ js/app.js
@@ -3,141 +3,141 @@ gsap.registerPlugin(ScrollTrigger);
 
 //   appOverviewSection
 gsap.to(".leftCard", {
-  scrollTrigger: {
-    trigger: ".leftCard",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -100,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".leftCard",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -100,
+    ease: "none",
+    duration: 3
 });
 
 gsap.to(".appOverviewSection_img", {
-  scrollTrigger: {
-    trigger: ".appOverviewSection_img",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -130,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".appOverviewSection_img",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -130,
+    ease: "none",
+    duration: 3
 });
 
 //   privacySection
 
 gsap.to(".privacySection_card", {
-  scrollTrigger: {
-    trigger: ".privacySection_card",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -90,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".privacySection_card",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -90,
+    ease: "none",
+    duration: 3
 });
 
 gsap.to(".privacySection_img", {
-  scrollTrigger: {
-    trigger: ".privacySection_img",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -160,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".privacySection_img",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -160,
+    ease: "none",
+    duration: 3
 });
 
 //   wrapSection
 
 gsap.to(".wrapSection_card", {
-  scrollTrigger: {
-    trigger: ".wrapSection_card",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -90,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".wrapSection_card",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -90,
+    ease: "none",
+    duration: 3
 });
 
 gsap.to(".wrapSection_img", {
-  scrollTrigger: {
-    trigger: ".wrapSection_img",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -160,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".wrapSection_img",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -160,
+    ease: "none",
+    duration: 3
 });
 
 //   performenceSection
 
 gsap.to(".performenceSection_card", {
-  scrollTrigger: {
-    trigger: ".performenceSection_card",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -90,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".performenceSection_card",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -90,
+    ease: "none",
+    duration: 3
 });
 
 // privacySection1
 gsap.to(".privacySection_card1", {
-  scrollTrigger: {
-    trigger: ".privacySection_card1",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -90,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".privacySection_card1",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -90,
+    ease: "none",
+    duration: 3
 });
 
 gsap.to(".privacySection_img1", {
-  scrollTrigger: {
-    trigger: ".privacySection_img1",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: "-70%",
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".privacySection_img1",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: "-70%",
+    ease: "none",
+    duration: 3
 });
 
 // devicesSection
 gsap.to(".devicesSection_card", {
-  scrollTrigger: {
-    trigger: ".devicesSection_card",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: -90,
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".devicesSection_card",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: -90,
+    ease: "none",
+    duration: 3
 });
 
 gsap.to(".devicesSection_img", {
-  scrollTrigger: {
-    trigger: ".devicesSection_img",
-    start: "top 80%",
-    end: "top 20%",
-    scrub: 1
-  },
-  y: "-80%",
-  ease: "none",
-  duration: 3
+    scrollTrigger: {
+        trigger: ".devicesSection_img",
+        start: "top 80%",
+        end: "top 20%",
+        scrub: 1
+    },
+    y: "-80%",
+    ease: "none",
+    duration: 3
 });
